484c1cadc52190d113a72eab0b45120efc396f3b
ealande <erickalan23@gmail.com>
2024-12-05 01:03:48 +0000

    new components created using NX

---
+++ apps/front/src/app/components/fotos/fotos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-fotos',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './fotos.component.html',
+  styleUrl: './fotos.component.css',
+})
+export class FotosComponent {}
