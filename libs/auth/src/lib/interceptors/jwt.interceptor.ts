258a59b39fe8e49d64681cfb9e5fd6ff44af55d6
ealande <erickalan23@gmail.com>
2024-12-05 00:48:14 +0000

    new theme

--- libs/auth/src/lib/interceptors/jwt.interceptor.ts
+++ libs/auth/src/lib/interceptors/jwt.interceptor.ts
@@ -1,5 +1,14 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { AuthService } from '../services/auth.service';
+import { inject } from '@angular/core';
 
 export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
+  const authService = inject(AuthService);
+
+  if(authService.jwt) {
+    return next(req.clone({
+      headers: req.headers.set('Authorization', `Bearer ${authService.jwt}` )
+    }))
+  }
   return next(req);
 };
