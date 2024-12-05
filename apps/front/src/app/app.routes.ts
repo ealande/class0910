57b0badabcd79d2658a7bb789b8aa8d9ee1fca74
ealande <erickalan23@gmail.com>
2024-12-05 01:12:14 +0000

    setting new routes

--- apps/front/src/app/app.routes.ts
+++ apps/front/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Route } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { SobreComponent } from './components/sobre/sobre.component';
+import { VideosComponent } from './components/videos/videos.component';
+import { FotosComponent } from './components/fotos/fotos.component';
 
 export const appRoutes: Route[] = [
   {
@@ -16,6 +18,16 @@ export const appRoutes: Route[] = [
     path: 'sobre',
     component: SobreComponent,
   },
+    {
+     title: 'Vídeos',
+     path: 'videos',
+     component: VideosComponent,
+    },
+    {
+      title: 'Fotos',
+      path: 'fotos',
+      component: FotosComponent,
+    },
   {
     path:'favorito-edicao',
     loadChildren: () => import('./modules/favorito-edicao/favorito-edicao.module',).then(m => m.FavoritoEdicaoModule),
